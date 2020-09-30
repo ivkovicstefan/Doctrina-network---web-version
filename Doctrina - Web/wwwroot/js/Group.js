f884f190fd166cc160cf9c3508f767b85a498166
Stefan Ivković <ivkovics@outlook.com>
2020-09-30 15:52:48 +0200

    Redesigned Home/Index.cshtml

--- Doctrina - Web/wwwroot/js/Group.js
+++ Doctrina - Web/wwwroot/js/Group.js
@@ -1,15 +1,19 @@
 ﻿function ShowGroupSettings() {
     document.getElementById("GroupSettingsContainer").style.display = "block";
+    document.getElementById("LayerDarker").style.display = "block";
 }
 
 function HideGroupSettings() {
     document.getElementById("GroupSettingsContainer").style.display = "none";
+    document.getElementById("LayerDarker").style.display = "none";
 }
 
 function ShowCreateScript() {
     document.getElementById("CreateScriptContainer").style.display = "block";
+    document.getElementById("LayerDarker").style.display = "block";
 }
 
 function HideCreateScript() {
     document.getElementById("CreateScriptContainer").style.display = "none";
+    document.getElementById("LayerDarker").style.display = "none";
 }
